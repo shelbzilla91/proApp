52235acfdc2271c1c448cd4b309a34ffcd9a2113
Shelby Baker <shelbymbaker91@gmail.com>
2019-06-28 14:13:34 -0500

    about to migrate

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,13 @@
+class DemoController < ApplicationController
+  layout false
+  def index
+    render ('index')
+  end
+  def hello
+    @array = [1,2,3,4,5]
+    render ('hello')
+  end
+  def other_hello
+    redirect_to(:action => 'hello')
+  end
+end
